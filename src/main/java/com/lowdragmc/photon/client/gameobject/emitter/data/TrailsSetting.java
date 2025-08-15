7288f3ef5a176c9fe36aa900e29af82c89d100ec
KilaBash <yefancy@foxmail.com>
2025-08-15 10:03:41 +0900

    Update TrailsSetting.java

--- src/main/java/com/lowdragmc/photon/client/gameobject/emitter/data/TrailsSetting.java
+++ src/main/java/com/lowdragmc/photon/client/gameobject/emitter/data/TrailsSetting.java
@@ -89,6 +89,8 @@ public class TrailsSetting extends ToggleGroup {
     public TrailsSetting() {
         config.setWidthOverTrail(NumberFunction.constant(0.5f));
         config.setParallelRendering(true);
+        araConfig.thickness = 0.5f;
+        araConfig.minDistance = 0.005f;
     }
 
     public void setup(ParticleEmitter emitter, TileParticle particle) {
